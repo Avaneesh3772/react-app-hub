32fcd3a01058812d3fac6774a2e7ce9cb36ceeae
Avaneesh Mishra <avaneesh.3772@gmail.com>
2024-08-25 13:48:45 +0530

    New React application

--- src/pages/JobLists/JobLists.tsx
+++ src/pages/JobLists/JobLists.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import jobs from '../../assets/mockdata/jobs.json'
+import Spinner from 'react-bootstrap/Spinner';
 import Job from './Job';
+import { JobType } from '../../models/models'
+
 
 const JobLists = () => {
+  const [jobInfo, setjobInfo] = useState([]);
+  const [loading, setLoading] = useState(true);
   console.log("jobs =>", jobs);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setjobInfo(jobInfo);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <div>
-        <h4>This is Job List Component</h4>       
-        {jobs.map((item:any, index) => (
-          <Job key={item.id} item={item} ></Job>
-        ))}      
+        <h4>This is Job List Component</h4> 
+
+        {loading ? (
+          <Spinner animation="border"/>         
+        ) : (
+          <div className='jobListings'>
+            {jobs.map((item:any, index) => (
+              <Job key={item.id} item={item} ></Job>
+             ))}  
+          </div>
+        )}            
     </div>
   )
 }
